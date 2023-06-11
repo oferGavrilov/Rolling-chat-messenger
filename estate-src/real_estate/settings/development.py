6697a87e443ed88d78340a39cac835f6e22754a9
ofer <ofergavri@gmail.com>
2023-06-11 19:08:02 +0300

    chore: split settings.py file

---
+++ estate-src/real_estate/settings/development.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
