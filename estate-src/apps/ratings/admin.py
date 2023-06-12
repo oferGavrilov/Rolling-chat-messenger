1a56f89c263bee32fced25d650e90568cca66ba4
ofer <ofergavri@gmail.com>
2023-06-12 13:06:02 +0300

    feat: add rating model and serializer

--- estate-src/apps/ratings/admin.py
+++ estate-src/apps/ratings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Rating
 
-# Register your models here.
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['rater', 'agent', 'rating', 'comment']
+
+admin.site.register(Rating, RatingAdmin)
