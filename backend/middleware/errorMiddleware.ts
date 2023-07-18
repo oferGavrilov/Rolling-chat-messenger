e3217f99115aa80ab1de31d49bf8868ed18d025f
ofer <ofergavri@gmail.com>
2023-07-18 22:33:50 +0300

    added error middleware

--- backend/middleware/errorMiddleware.ts
+++ backend/middleware/errorMiddleware.ts
@@ -6,13 +6,15 @@ export function notFound (req: Request, res: Response, next: NextFunction) {
       next(error)
 }
 
-export function errorHandler (err: any, res: Response) {
+export function errorHandler (err: any, req: Request, res: Response, next: NextFunction) {
       const statusCode = res.statusCode === 200 ? 500 : res.statusCode
       res.status(statusCode)
       res.json({
             message: err.message,
             stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack
       })
+
+      next();
 }
 
 interface ErrorResponse {
